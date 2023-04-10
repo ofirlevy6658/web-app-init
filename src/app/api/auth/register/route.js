4eaa8ab5a78f7b348788f44ccd70c5842aecb78c
ofirlevy6658 <ofirlevy6658@gmail.com>
2023-04-10 21:57:31 +0300

    update

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,6 +1,6 @@
 export async function POST(req, res) {
     const body = await req.json()
-    console.log('test')
+    console.log('tsest')
     // res.status(200).json({message: 'ok'})
     return new Response(JSON.stringify(body))
 }
