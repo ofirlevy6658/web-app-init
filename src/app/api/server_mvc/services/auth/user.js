fe1f6e49ff293a1af8e724b12e1b01313342660f
vitaly <vitalypliss641991@gmail.com>
2023-07-03 17:44:43 +0300

    fixing build

--- src/app/api/server_mvc/services/auth/user.js
+++ src/app/api/server_mvc/services/auth/user.js
@@ -1,8 +1,8 @@
-import {connectMongo} from '../../../../../../utils/db.js'
+// import {connectMongo} from '../../../../../../utils/db.js'
 import User from '../../models/auth/user.js'
 
 export const createUser = async (userBody) => {
-    await connectMongo()
+    // await connectMongo()
     const user = new User(userBody)
     console.log('user', user)
 
